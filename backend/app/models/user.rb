e0f5a5bc3d8b0c9329780ff390e923f53c174617
Onkar Dhillon <onkardhillon73@gmail.com>
2026-03-04 02:12:39 -0500

    Adding error message.

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   has_many :applications, dependent: :destroy
   after_create :create_applicant_profile
   has_one :applicant_profile, dependent: :destroy
+  validates :email_address, uniqueness: true
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
 end
