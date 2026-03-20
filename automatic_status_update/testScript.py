e81142041199b9c975ffba80652e5b2a8ac15446
Onkar Dhillon <onkardhillon73@gmail.com>
2026-03-19 22:07:33 -0400

    Added a requirements doc to doucment the dependencies used.

--- automatic_status_update/testScript.py
+++ automatic_status_update/testScript.py
@@ -1,15 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+import os
 
-EMAIL = 'onkardhillon73@gmail.com'
-PASSWORD = 'password123'
+load_dotenv()
 
-PROTECTED_URL = "https://gradapp.wpi.edu/apply/status"
+EMAIL = os.getenv("EMAIL")
+PASSWORD = os.getenv("PASSWORD")
+
+PROTECTED_URL = lambda portal_link: f"{portal_link.split('/account')[0]}/apply/status"
 
 
-LOGIN_API = "http://localhost:4000/api/session"
-APPLICATIONS_API = "http://localhost:4000/api/applications"
-UPDATE_APPLICATIONS_API = lambda app_id: f"http://localhost:4000/api/applications/{app_id}"
+LOGIN_API = os.getenv("LOGIN_API")
+APPLICATIONS_API = os.getenv("APPLICATIONS_API")
+UPDATE_APPLICATIONS_API = lambda app_id: f"{APPLICATIONS_API}/{app_id}"
 
 def get_jwt(payload,login_route):
 
@@ -52,22 +56,19 @@ def scrape_status(app):
 
         session.get(login_route, data=payload)
 
-        protected_page_response = session.get(PROTECTED_URL)
+        protected_page_response = session.get(PROTECTED_URL(login_route))
 
         soup = BeautifulSoup(protected_page_response.content, 'html.parser')
 
-        status = soup.find('strong', string=lambda t: 'Current Application Status' in t).next_sibling
+        try:
+
+            status = soup.find('strong', string=lambda t: 'Current Application Status' in t).next_sibling
+        except AttributeError:
+            status = "DNE"
+
 
     return status.strip()
 
-def get_old_statuses(apps):
-
-    old_statuses = []
-
-    for app in apps:
-        old_statuses.append(app['status'])
-
-    return old_statuses
 
 def is_status_change(new_status, old_status):
 
