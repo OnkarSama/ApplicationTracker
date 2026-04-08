9549f29dbc515b1b1e760fc58c42aade68f50c26
Gabriel Kaloo <gkaloo17591@gmail.com>
2026-04-08 11:54:21 -0400

    womp

--- backend/db/migrate/20260323043441_remove_notes_column_from_applications.rb
+++ backend/db/migrate/20260323043441_remove_notes_column_from_applications.rb
@@ -1,5 +1,5 @@
 class RemoveNotesColumnFromApplications < ActiveRecord::Migration[8.1]
     def change
-      remove_column :applications, :notes, :text
+        remove_column :applications, :notes, :text, if_exists: true
     end
 end
