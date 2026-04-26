b3b96e2fd10161fcc9b9dd6ae305e1c103f9bc00
Onkar Dhillon <onkardhillon73@gmail.com>
2026-04-26 10:50:54 -0400

    Removed the notes from the extension.

--- browser-extension/src/api/application.ts
+++ browser-extension/src/api/application.ts
@@ -5,11 +5,19 @@ export type ApplicationPayload = {
         id: number,
         company: string,
         position?: string,
-        notes: string,
         status: string | "Applied",
         priority: string,
         category: string,
     }
+}
+
+export type CreateApplicationPayload = {
+    company: string
+    position?: string
+    status?: string
+    priority?: string
+    category?: string
+    notes?: string
 }
 
 export type ApplicationCredential = {
@@ -34,6 +42,11 @@ const endpoints = {
     getApplications: async () => {
         const response = await api("/applications");
         return response.applications;
+    },
+
+    createApplication: async (payload: CreateApplicationPayload) => {
+        const response = await api("/applications", { method: "post", data: { application: payload } });
+        return response;
     },
 
 }
