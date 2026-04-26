64a7a906ae034fc32056ed1fd56d8e258b72e96c
Onkar Dhillon <onkardhillon73@gmail.com>
2026-04-26 10:49:04 -0400

    Added the filter to the jobs sent to the sync script.

---
+++ backend/app/services/automatic_status_update_service.rb
@@ -0,0 +1,33 @@
+require 'net/http'
+require 'uri'
+require 'json'
+
+class AutomaticStatusUpdateService
+
+    def self.requestUpdate(user, notify = false)
+
+        uri = URI.parse(ENV['AUTOMATIC_STATUS_UPDATE_API_ROUTE'])
+        api_key = ENV['AUTOMATIC_STATUS_UPDATE_API_KEY']
+
+        applications = user.applications.includes(:application_credential).where.not(status: ["Rejected", "Offer"])
+
+        Net::HTTP.start(uri.hostname, uri.port) do |http|
+
+            headers = {
+                'Content-Type' => 'application/json',
+                'Authorization' => "Bearer #{api_key}"
+            }
+
+            request = Net::HTTP::Post.new(uri.request_uri, headers)
+            request.body = JSON.generate(applications.as_json(include: :application_credential))
+
+            response = http.request(request)
+
+            if notify
+                parsedData = JSON.parse(response.body)
+                return parsedData["Updated"], parsedData["applications"] || []
+            end
+        end
+    end
+
+end
